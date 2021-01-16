9fbbf8eb2fdcd1ab96ef2b0a7bf1b27a79f7bd9f
Tech Student <razaqhammed36@gmail.com>
2021-01-16 09:25:24 -0500

    Added some more widgets
    New Widgets:
        - A text input box that has multiline functionallity
        - A push button
    
    All of the widgets (old or new) are based on the Widget class I defined in the __init__.py that's in the widgets folder.

--- components/widgets/__init__.py
+++ components/widgets/__init__.py
@@ -1,12 +1,88 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
-class Textbox(Label):
-	def __init__(self, surface=None, text=None, **kwargs):
-		super().__init__(surface, text=text, **kwargs)
-
-		self.surface = surface
-		self.text = text
+class Widget:
+	def __init__(self, surface=None, **kwargs):
+		self.master = surface
 		self.options = kwargs
 
+	def _create(self, widget, x=0, y=0, **kwargs):
+		self.x = x
+		self.y = y
+
+		self.surface = Frame(self.master)
+
+		self.widget = widget(self.surface, **kwargs)
+		self.widget.pack()
+
+		self.surface.place(x=self.x, y=self.y)
+
 	def center(self):
-		self.place(x=0, y=0, relx=0.5, rely=0.5, anchor="center")
+		self.surface.place(x=0, y=0, relx=0.5, rely=0.5, anchor="center")
+
+class Textbox(Widget):
+	def __init__(self, surface=None, text=None, x=0, y=0, **kwargs):
+		super().__init__(surface)
+
+		self.master = surface
+		self.text = text
+
+		self.x = x
+		self.y = y
+
+		self.options = kwargs
+
+		self._create(Label, self.x, self.y, text=self.text, **kwargs)
+
+class TextInputBox(Widget):
+	def __init__(self, surface=None, multiline=False, x=0, y=0, **kwargs):
+		super().__init__(surface)
+
+		self.master = surface
+		self.multiline = multiline
+
+		self.x = x
+		self.y = y
+
+		self.options = kwargs
+
+		if self.multiline:
+			self._create(ScrolledText, self.x, self.y, **kwargs)
+		else:
+			self.stringVar = StringVar()
+			self._create(Entry, self.x, self.y, textvariable=self.stringVar, **kwargs)
+
+	def get(self, start=1.0, end=END):
+		if self.multiline:
+			output = self.widget.get(start, end)
+		else:
+			output = self.widget.get()
+
+		return output
+	
+	def insert(self, text="", index="1.0"):
+		if self.multiline:
+			self.widget.insert(index, text)
+		else:
+			self.widget.insert(0, text)
+
+	def clear(self):
+		if self.multiline:
+			self.widget.delete(1.0, END)
+		else:
+			self.widget.delete(0, END)
+
+class PushButton(Widget):
+	def __init__(self, surface=None, text=None, command=lambda: print(""), x=0, y=0, **kwargs):
+		super().__init__(surface)
+
+		self.master = surface
+		self.text = text
+		self.command = command
+
+		self.x = x
+		self.y = y
+
+		self.options = kwargs
+
+		self._create(Button, self.x, self.y, text=self.text, command=self.command, **kwargs)
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from components import Main, Screen
-from components.widgets import Textbox
+from components.widgets import Textbox, TextInputBox, PushButton
 
 class MainScreen(Screen):
 	def build(self):
-		textbox = Textbox(self, text="I am in a class!")
+		Textbox(self, text="Enter some text", x=850, y=200, font=('segoe ui', 20))
+		textbox = TextInputBox(self, multiline=True)
+		textbox.insert("Hello!")
+		textbox.clear()
 		textbox.center()
+
+		button = PushButton(self, text="Get Input", x=900, y=800, command=lambda: Textbox(self, text=textbox.get(), x=900, y=900))
 
 class ManOS(Main):
 	def build(self):
